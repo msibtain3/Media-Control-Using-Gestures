76ab399a7a6dfa070919760b51f6aee28a0cdf99
Sibtain Reza <43268995+msibtain3@users.noreply.github.com>
2021-09-21 07:36:07 +1000

    Update vlc_instance.py

--- vlc_instance.py
+++ vlc_instance.py
@@ -2,5 +2,5 @@ import vlc
 
 Instance = vlc.Instance()
 player = Instance.media_player_new()
-Media = Instance.media_new("C:/Users/MSIBT/Downloads/Video/Tom & Jerry - Top 10 Classic Chase Scenes - Classic Cartoon - WB Kids - YouTube.mkv")    
+Media = Instance.media_new("path to the video file")    
 player.set_media(Media)
